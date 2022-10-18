3a31707a0e061143d8ce7cb92b27ad57dd68d0f2
iqbalatma <iqbalatma@gmail.com>
2022-10-18 22:05:32 +0700

    show and index asset done

--- app/Http/Controllers/API/AssetController.php
+++ app/Http/Controllers/API/AssetController.php
@@ -2,13 +2,52 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Http\Resources\AssetResource;
+use App\Http\Resources\AssetResourceCollection;
+use App\Services\AssetService;
+use Illuminate\Http\JsonResponse;
 
-class AssetController extends Controller
+class AssetController extends ApiController
 {
-    public function index()
+    private string $responseName = 'Assets';
+    private array $responseMessage = [
+        'index' => 'Get all assets successfully',
+        'show' => 'Get asset successfully',
+        // 'store' => 'Store organization successfully',
+        // 'update' => 'Update organization successfully',
+        // 'destroy' => 'Delete organization successfully',
+        // 'destroy_failed' => 'Delete congress day failed, the data is not exists',
+    ];
+
+
+    /**
+     * Description : use for get all data index
+     * 
+     * @param AssetService $service for execute logic
+     * @return JsonResponse for response api
+     */
+    public function index(AssetService $service):JsonResponse
     {
-        
+        $data = $service->getAllData();
+
+        return $this->responseWithResourceCollection(
+            new AssetResourceCollection($data),
+            $this->responseName,
+            $this->responseMessage['index'],
+            JsonResponse::HTTP_OK
+        );
+    }
+
+    public function show(AssetService $service, int $id):JsonResponse
+    {
+        $data = $service->getDataById($id);
+
+        return $this->responseWithResource(
+            new AssetResource($data),
+            $this->responseName,
+            $this->responseMessage['show'],
+            JsonResponse::HTTP_OK
+        );
     }
     
     public function download(int $id)
