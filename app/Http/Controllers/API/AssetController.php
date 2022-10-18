d200a87defc59ae4eaca120204105f29ec950937
iqbalatma <iqbalatma@gmail.com>
2022-10-18 21:47:05 +0700

    asset migration and model

---
+++ app/Http/Controllers/API/AssetController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+
+class AssetController extends Controller
+{
+    public function index()
+    {
+        
+    }
+    
+    public function download(int $id)
+    {
+    }
+}
