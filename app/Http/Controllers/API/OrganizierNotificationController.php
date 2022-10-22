3cd61606e2e85539bfa56fa751a3c6f9fdb61348
iqbalatma <iqbalatma@gmail.com>
2022-10-22 14:15:41 +0700

    notification show

--- app/Http/Controllers/API/OrganizierNotificationController.php
+++ app/Http/Controllers/API/OrganizierNotificationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Resources\OrganizierNotificationResource;
 use App\Http\Resources\OrganizierNotificationResourceCollection;
 use App\Services\OrganizierNotificationService;
 use Illuminate\Http\JsonResponse;
@@ -26,6 +27,10 @@ class OrganizierNotificationController extends ApiController
     public function latest(OrganizierNotificationService $service):JsonResponse
     {
         $data = $service->getLatestData();
-        return response()->json(['data'=>$data]);    
+        return $this->responseWithResource(
+            new OrganizierNotificationResource($data),
+            $this->responseName,
+            $this->responseMessage['index'],
+            JsonResponse::HTTP_OK);
     }
 }
