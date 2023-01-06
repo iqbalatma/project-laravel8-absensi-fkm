150b822c137e6f4bb8f4f3596b8708669d2f4f08
iqbalatma <iqbalatma@gmail.com>
2023-01-06 17:11:06 +0700

    create auth controller

---
+++ app/Http/Controllers/API/v1/Auth/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Auth;
+
+use App\Http\Controllers\API\v1\ApiController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends ApiController
+{
+
+    public function authenticate()
+    {
+    }
+}
