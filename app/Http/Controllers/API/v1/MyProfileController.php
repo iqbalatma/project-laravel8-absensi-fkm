96796245e206d0ca8c3449d5d1b20ed649bd9fa3
iqbalatma <iqbalatma@gmail.com>
2023-01-11 01:28:00 +0700

    add new stub file

---
+++ app/Http/Controllers/API/v1/MyProfileController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MyProfileController extends Controller
+{
+    public function show()
+    {
+        # code...
+    }
+}
