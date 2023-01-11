03a96587658cfdaa40675beec286a2c2f6cf73d8
iqbalatma <iqbalatma@gmail.com>
2023-01-11 14:47:56 +0700

    my profile feature done

--- app/Http/Controllers/API/v1/MyProfileController.php
+++ app/Http/Controllers/API/v1/MyProfileController.php
@@ -2,13 +2,35 @@
 
 namespace App\Http\Controllers\API\v1;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\MyProfile\UpdateMyProfileRequest;
+use App\Http\Resources\MyProfile\MyProfileResource;
+use App\Services\MyProfileService;
+use Illuminate\Http\JsonResponse;
 
-class MyProfileController extends Controller
+class MyProfileController extends ApiController
 {
-    public function show()
+    private string $responseName = "My Profile";
+    private array $responseMessage = [
+        "show" => "Get my profile successfully",
+        "update" => "Update my profile successfully"
+    ];
+    public function show(MyProfileService $service): JsonResponse
     {
-        # code...
+        return $this->responseWithResource(
+            new MyProfileResource($service->getDataById()),
+            $this->responseName,
+            $this->responseMessage["show"],
+            JsonResponse::HTTP_OK
+        );
+    }
+
+    public function update(MyProfileService $service, UpdateMyProfileRequest $request): JsonResponse
+    {
+        return $this->responseWithResource(
+            new MyProfileResource($service->updateDataById($request->validated())),
+            $this->responseName,
+            $this->responseMessage["update"],
+            JsonResponse::HTTP_OK
+        );
     }
 }
--- app/Services/MyProfileService.php
+++ app/Services/MyProfileService.php
@@ -1,1 +1,47 @@
 <?php
+
+namespace App\Services;
+
+use App\Exceptions\EmptyDataException;
+use App\Repositories\UserRepository;
+use App\Statics\Table;
+use Illuminate\Support\Facades\Auth;
+
+class MyProfileService extends BaseService
+{
+    protected $repository;
+    private const USER_SELECT_COLUMN = [
+        Table::USER . ".id",
+        Table::USER . ".name",
+        Table::USER . ".personal_token",
+        Table::USER . ".email",
+        Table::USER . ".student_id",
+        Table::USER . ".generation",
+        Table::USER . ".phone_number",
+        Table::ROLE . ".name as role_name",
+        Table::ORGANIZATION . ".name as organization_name",
+        Table::ORGANIZATION . ".shortname as organization_shortname",
+        Table::USER . ".updated_at",
+    ];
+
+    public function __construct()
+    {
+        $this->repository = new UserRepository();
+    }
+
+    public function getDataById(): object
+    {
+        $data = $this->repository->getDataById(Auth::id(), self::USER_SELECT_COLUMN);
+        if (empty($data)) {
+            throw new EmptyDataException();
+        }
+
+        return $data;
+    }
+
+    public function updateDataById(array $requestedData)
+    {
+        $this->checkData(Auth::id());
+        return $this->repository->updateDataById(Auth::id(), $requestedData);
+    }
+}
